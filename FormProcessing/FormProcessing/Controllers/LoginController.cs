7062784dfd959536f751d1d54672cd13c9e60b28
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-11-06 13:49:28 +0600

    Form Submission

---
+++ FormProcessing/FormProcessing/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using FormProcessing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormProcessing.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var name = Request["Username"];
+            var log = new Login();
+            return View(log);
+        }
+        //[HttpPost]
+        //public ActionResult Index(FormCollection fc)
+        //{
+        //    var name = fc["Username"];
+        //    return View();
+        //}
+        [HttpPost]
+        public ActionResult Index(Login log)
+        {
+            if (ModelState.IsValid) { //validation
+                return RedirectToAction("Index","Home");   
+            }
+            return View(log);
+        }
+        //[HttpPost]
+        //public ActionResult Index(string Username, string Password)
+        //{
+        //    var name = Username;
+        //    return View();
+        //}
+    }
+}
---
+++ FormProcessing/FormProcessing/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormProcessing.Models
+{
+    public class Login
+    {
+        [Required (ErrorMessage ="Provide Username")]
+        [StringLength(6,ErrorMessage ="Username must not exceed 6 ")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength (50,MinimumLength =8)]
+        public string Password { get; set; }
+    }
+}
