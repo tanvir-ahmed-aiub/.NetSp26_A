3d403ad2a90b70a5d1f0993a04ba196e586c8d30
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-12-02 14:53:25 +0600

    Sample Project

---
+++ PMS/PMS/CustomValidation/ConfPassValidate.cs
@@ -0,0 +1,24 @@
+﻿using PMS.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PMS.CustomValidation
+{
+    public class ConfPassValidate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var cus = validationContext.ObjectInstance as CustomerDTO;
+            if (cus.Password != null && value != null)
+            {
+                if (cus.Password.Equals(value.ToString())) {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult("Confirm Password not Matched");
+        }
+    }
+}
