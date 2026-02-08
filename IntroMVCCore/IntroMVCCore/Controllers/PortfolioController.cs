62fa58bfcf99b3d720e04728fb86643a98866ba0
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2026-02-08 12:50:53 +0600

    Introduction

---
+++ IntroMVCCore/IntroMVCCore/Controllers/PortfolioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroMVCCore.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+        public IActionResult Education() { 
+            return View();
+        }
+    }
+}
